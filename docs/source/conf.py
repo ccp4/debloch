eb08cd2c6014687a137f3dcb6b17e74dd04d5094
tdrevon <ronandrevon@gmail.com>
2022-03-08 11:27:13 +0100

    conf update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ author = 'tarik drevon'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
